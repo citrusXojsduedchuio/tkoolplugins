89e152cfb9d680697b59adb26322a6caf3aa883f
citrusXojsduedchuio <44925974+citrusXojsduedchuio@users.noreply.github.com>
2021-03-02 21:53:10 +0900

    Update CTRS_MZ_TpbSpeedPlusMinus.js
    ダウンロードリンクを修正

--- CTRS_MZ_TpbSpeedPlusMinus.js
+++ CTRS_MZ_TpbSpeedPlusMinus.js
@@ -40,7 +40,7 @@
 ですができれば、ゲーム内などに名前を表示してくれるとありがたいです。
 
 @url
-https://www.dropbox.com/sh/30u0e9goi4yd17n/AAAaufl3dIPJPIXRT1-_wFEUa?dl=0
+https://github.com/citrusXojsduedchuio/tkoolplugins/blob/main/CTRS_MZ_TpbSpeedPlusMinus.js
 */
 (() => {
 	'use strict'
