3555ba3bb1c73450787c33d678ad986d759dcf15
citrusXojsduedchuio <44925974+citrusXojsduedchuio@users.noreply.github.com>
2021-03-14 09:08:14 +0900

    ダウンロードURLを更新

--- CTRS_MZ_TpgaugeRestartPoint.js
+++ CTRS_MZ_TpgaugeRestartPoint.js
@@ -23,7 +23,7 @@
 ですができれば、ゲーム内などに名前を表示してくれるとありがたいです。
 
 @url
-https://www.dropbox.com/sh/30u0e9goi4yd17n/AAAaufl3dIPJPIXRT1-_wFEUa?dl=0
+https://github.com/citrusXojsduedchuio/tkoolplugins/blob/main/CTRS_MZ_TpgaugeRestartPoint.js
 */
 (() => {
 	//再定義して、tpbReStartPointを追加する
