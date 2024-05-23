d697bfd89cefdd175f9d232974c186d666c45181
citrusXojsduedchuio <44925974+citrusXojsduedchuio@users.noreply.github.com>
2024-05-24 07:04:41 +0900

    Update CTRS_MZ_EquipHeight.js

--- CTRS_MZ_EquipHeight.js
+++ CTRS_MZ_EquipHeight.js
@@ -22,8 +22,7 @@
 ですができれば、ゲーム内などに名前を表示してくれるとありがたいです。
 
 @url
-https://www.dropbox.com/sh/30u0e9goi4yd17n/AAAaufl3dIPJPIXRT1-_wFEUa?dl=0
-*/
+https://github.com/citrusXojsduedchuio/tkoolplugins/blob/main/CTRS_MZ_EquipHeight.js
 (() => {
 	let divide = Number(PluginManager.parameters("CTRS_MZ_EquipHeight").equipDivide);
 	if(isNaN(divide) ){
